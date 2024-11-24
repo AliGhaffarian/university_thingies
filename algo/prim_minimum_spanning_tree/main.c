b938fe4fb4693aa544a18babc7c3097bed7b8de9
AliGhaffarian <alighaffarian9@gmail.com>
2024-11-24 19:59:24 +0330

    handle error where prim_minimum_spanning_tree() returned null pointer

--- algo/prim_minimum_spanning_tree/main.c
+++ algo/prim_minimum_spanning_tree/main.c
@@ -1,6 +1,8 @@
 #include "graph_ops.h"
 #include "prim.h"
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
 		int graph_size = 0;
 		int **graph = generate_random_graph(&graph_size);
@@ -9,6 +11,8 @@ int main(){
 		show_graph(graph, graph_size, 1);
 
 		int **mst = prim_minimum_spanning_tree(graph, graph_size);
+		if(mst == (int **)0)
+				exit(EINVAL);
 		printf("\n\n\n");
 		
 		printf("minimum spanning tree:\n");
