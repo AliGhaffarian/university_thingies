d4bd69157940f5ccd16f58ea4a5d73f49f7c6099
user <alighaffarian9@gmail.com>
2025-05-18 12:49:32 +0330

    add debug options

--- ai/nqueens/genetic.c
+++ ai/nqueens/genetic.c
@@ -3,12 +3,24 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <sys/param.h>
 #define POP_SIZE 100
 #define N 8
 #define POP_MEM_SIZE (N * POP_SIZE * sizeof(int))
 #define MAX_COST (((N - 1 + 1) / 2) * (N - 1))
 
 #define debug_delim "--------------"
+
+//#define DEBUG 
+//#define MAX_ITER 100
+
+#ifdef DEBUG 
+#include <stdio.h>
+#define dbg_printf printf
+#else
+#define dbg_printf(...)
+#endif
+
 int cost(int *solution){
 	int result = 0;
 	for(int i = 0; i < N; i++)
@@ -54,27 +66,29 @@ int *make_parent_index_pool_based_on_fitness(int *population, int *size_of_pool)
 	// 				 			number of solutions 	* MAX_COST to make room for max fitnes
 	int *parent_index_pool_based_on_fitness= (int *)malloc(POP_SIZE  		* MAX_COST * sizeof(int));
 	int current_pool_index = 0;
+	int max_fitness = 0;
 	for (int i = 0; i < N; i++){
 		int fitness_of_i = (MAX_COST) - cost(population + (i * N));
 		for (int j = current_pool_index; j < current_pool_index + fitness_of_i; j++){
 			parent_index_pool_based_on_fitness[j] = i;
 		}
+		max_fitness = MAX(max_fitness, fitness_of_i);
 		current_pool_index += fitness_of_i;
 	}
+	dbg_printf("max fitness: %d\n", max_fitness);
+	float average_fitness = (float)current_pool_index / N;
+	dbg_printf("average fitness: %f\n", average_fitness);
 	(*size_of_pool) = current_pool_index;
 	return parent_index_pool_based_on_fitness;
 }
-int *select_parents(int *population){
+int *select_parents(int *population, int *parent_index_pool_based_on_fitness, int size_of_parent_pool){
 	//int parent_indexes[N][2]
 	int *parent_indexes = (int *)malloc(2 * sizeof(int)); //two parent for each entry
 
-	int size_of_parent_pool;
-	int *parent_index_pool_based_on_fitness = make_parent_index_pool_based_on_fitness(population, &size_of_parent_pool);
 	for (int i = 0; i < 2; i++){
 		int random_index_for_map = random() % size_of_parent_pool;
 		parent_indexes[i] = parent_index_pool_based_on_fitness[random_index_for_map];
 	}
-	free(parent_index_pool_based_on_fitness);
 	return parent_indexes;
 }
 int *crossover_and_mutate(int *first_parent, int *second_parent){
@@ -86,12 +100,12 @@ int *crossover_and_mutate(int *first_parent, int *second_parent){
 		offsprings[i + N] = ( i >= crossover_line ) ? first_parent[i] : second_parent[i];
 	}
 
-	bool do_mutate = random() % 2;
+	bool do_mutate = (random() % 10) < 3;
 	if (do_mutate){
 		offsprings[random_y()] = random_y();
 	}
 
-	do_mutate = random() % 2;
+	do_mutate = (random() % 10) < 3;
 	if (do_mutate){
 		offsprings[random_y() + N] = random_y();
 	}
@@ -101,8 +115,9 @@ int *iter_ga(int *population){
 	int *result_population = (int *)malloc(POP_MEM_SIZE);
 
 	int size_of_parent_pool;
+	int *parent_index_pool_based_on_fitness = make_parent_index_pool_based_on_fitness(population, &size_of_parent_pool);
 	for(int i = 0; i < POP_SIZE; i += 2){
-		int *parent_indexes = select_parents(population);
+		int *parent_indexes = select_parents(population, parent_index_pool_based_on_fitness, size_of_parent_pool);
 		int *children = crossover_and_mutate(
 				population + (parent_indexes[0] * N), 
 				population + (parent_indexes[1] * N));
@@ -113,6 +128,7 @@ int *iter_ga(int *population){
 		free(children);
 		free(parent_indexes);
 	}
+	free(parent_index_pool_based_on_fitness);
 	return result_population;
 }
 int *solve_ga(){
@@ -120,11 +136,17 @@ int *solve_ga(){
 	int *solution;
 	int i = 0;
 	while ((solution = solution_among_population(population)) == 0) {
+
+#ifdef MAX_ITER
+		if (i == MAX_ITER) return solution;
+#endif
+		dbg_printf("generation: %d\n", i);
 		free(solution);
 		int *next_gen = iter_ga(population);
 		free(population);
 		population = next_gen;
 		i++;
+		dbg_printf("%s\n",debug_delim);
 	}
 	printf("solved in %d gens\n", i);
 	return solution;
@@ -132,6 +154,8 @@ int *solve_ga(){
 int main(){
 	srand(time(0));
 	int *solution = solve_ga();
+	if (solution == 0)return 0;
+
 	for(int i = 0; i < N; i++){
 		printf("%d, ", solution[i]);
 	}
